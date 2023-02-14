46cf1382c992eb406991d2f7ef78a29945646c96
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-02-14 14:35:39 +0200

    Add project files.

---
+++ interface practice/Array.cs
@@ -0,0 +1,167 @@
+﻿using Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayClass
+{
+    public class MyArray : IOutput, IMath, ISort
+    {
+       private int[] arr; 
+        private int size;
+        public int Size
+        {
+            get { return size; }
+            set
+            {
+                try
+                {
+                    if (size < 0) throw new ArgumentException();
+                    size = value;
+                    arr = new int[size];
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                    Environment.Exit(1);
+                }
+            }
+        }
+
+        public MyArray()
+        {
+            size = 1;
+            arr = new int[1];
+        }
+        public MyArray(int size)
+        {
+            this.size = size;
+            arr = new int[size];
+        }
+
+        public int this[int i]
+        {
+            get 
+            {
+                return arr[i];
+            }
+            set
+            {
+                try
+                {
+                    if(i>size) throw new ArgumentException();
+                    arr[i] = value;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                    Environment.Exit(1);
+                }
+            }
+        }
+        public void Show()
+        {
+            foreach(var i in arr){
+                Console.Write("{0} ",i);
+            }
+            Console.WriteLine();
+        }
+
+        public void Show(string info)
+        {
+            Show();
+            Console.WriteLine("{0}\n",info);
+        }
+
+        public int Max()
+        {
+            int max = arr[0];     
+            for(int i=0; i < arr.Length; i++)
+            {
+                if(arr[i] > max) max = arr[i];
+            }
+            return max;
+        }
+
+        public int Min()
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min) min = arr[i];
+            }
+            return min;
+        }
+
+        public float Avg()
+        {
+            float sum = 0;
+            for(int i=0; i<arr.Length; i++)
+            {
+                sum+= arr[i];
+            }
+            return sum/arr.Length;
+        }
+
+        public bool Search(int ValueToSearch)
+        {
+            for(int i=0; i<arr.Length; i++)
+            {
+                if (arr[i] == ValueToSearch) return true;
+            }
+            return false;
+        }
+
+        public void SortAsc()
+        {
+            if (size <= 1) return;
+            int index = 0;
+            while (index < arr.Length)
+            {
+                if (index == 0)
+                    index++;
+                if (arr[index] <= arr[index - 1])
+                    index++;
+                else
+                {
+                    (arr[index], arr[index - 1]) = (arr[index-1], arr[index]);
+                    index--;
+                }
+            }
+        }
+
+        public void SortDesc()
+        {
+            if (size <= 1) return;
+            int index = 0;
+            while (index < arr.Length)
+            {
+                if (index == 0)
+                    index++;
+                if (arr[index] >= arr[index - 1])
+                    index++;
+                else
+                {
+                    (arr[index], arr[index - 1]) = (arr[index - 1], arr[index]);
+                    index--;
+                }
+            }
+        }
+
+        public void SortByParam(bool isAsc)
+        {
+            if (isAsc == true)
+            {
+                SortAsc();
+            }
+            else SortDesc();
+        }
+    }
+}
---
+++ interface practice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArrayClass;
+
+namespace interface_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray arr = new MyArray(4);
+            Console.WriteLine(arr.Size);
+            arr[0] = 6;
+            arr[1] = 2;
+            arr[2] = 3;
+            arr[3] = 4;
+            arr.Show();
+            Console.WriteLine("----------------------\n");
+            arr.Show("Message");
+            Console.WriteLine(arr.Avg());
+            Console.WriteLine(arr.Max());
+            Console.WriteLine(arr.Min());
+            Console.WriteLine(arr.Search(2));
+            Console.WriteLine(arr.Search(3));
+            arr.SortDesc();
+            arr.Show();
+            arr.SortAsc();
+            arr.Show();
+            Console.ReadKey();
+        }
+    }
+}
